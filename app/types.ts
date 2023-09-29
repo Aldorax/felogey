330e9ab6e024f9f0cabdee09ed0e52dda2f4856b
Aldorax <climaxating@gmail.com>
2023-09-29 12:36:23 +0100

    changes

--- app/types.ts
+++ app/types.ts
@@ -3,14 +3,14 @@ export interface User {
   first_name: string;
   last_name: string;
   email: string;
-  phone_number: string;
+  phone_number: string | null;
   role: string;
   created_at: string;
   is_email_verified: string;
   profile_image: string; // Change the type to include "null"
   modified_at: string;
-  account: number;
-  bank_name: string;
+  account: number | null;
+  bank_name: string | null;
   has_paid: string;
   referral_list: ReferredUser[];
   earnings: number | null;
@@ -18,11 +18,7 @@ export interface User {
   referral_link: string;
   referred_me: string;
   total_referred_users: number;
-  total_paid_users: number;
-  total_unpaid_users: number;
   mobilizer_intern_id: string;
-  total_users: number; // Total registered users
-  // recent_referral_history: ReferralHistory[]; // Recent referral history
 }
 
 export interface ReferredUser {
